d603fc3f1e04e274906c6e02d5d61eb58a352fe6
cebed <36077080+cebed@users.noreply.github.com>
2019-03-26 23:08:55 +0100

    codebody

---
+++ src/main/java/com/example/resumegeneratorbackend/ResumegeneratorBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.resumegeneratorbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumegeneratorBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResumegeneratorBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/resumegeneratorbackend/ResumegeneratorBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.resumegeneratorbackend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ResumegeneratorBackendApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
