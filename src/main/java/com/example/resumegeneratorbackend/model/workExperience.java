ddd9b40df3fa3208ec9db108c06dad3ff084cb69
nurhusein <nurabd@student.chalmers.se>
2019-04-01 23:44:30 +0200

    workexpiriernce columns

--- src/main/java/com/example/resumegeneratorbackend/model/workExperience.java
+++ src/main/java/com/example/resumegeneratorbackend/model/workExperience.java
@@ -1,4 +1,35 @@
 package com.example.resumegeneratorbackend.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 public class workExperience {
+
+
+    @Id
+    @Getter
+    @Setter
+    @GeneratedValue
+    @Column(name = "id")
+    private Integer id;
+    @Getter
+    @Setter
+    @Column(name = "title")
+    private String title;
+    @Getter
+    @Setter
+    @Column(name = "start_date")
+    private String start_date;
+    @Getter
+    @Setter
+    @Column(name = "end_date")
+    private Integer end_date;
+    @Getter
+    @Setter
+    @Column(name = "description")
+    private Integer description;
 }
