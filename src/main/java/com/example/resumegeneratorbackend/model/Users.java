7c9ddc081a94b27ff0a2b73f717a45e50c973b62
nurhusein <nurabd@student.chalmers.se>
2019-04-02 15:04:11 +0200

    able to reciva a string from front end and put it to database

--- src/main/java/com/example/resumegeneratorbackend/model/Users.java
+++ src/main/java/com/example/resumegeneratorbackend/model/Users.java
@@ -32,4 +32,28 @@ public class Users {
     public void setEmail(String email) {
         this.email = email;
     }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
