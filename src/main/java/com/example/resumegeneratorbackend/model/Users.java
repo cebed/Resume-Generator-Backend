24507fc6f41abebb4a3683c1bae6c71d3c40c18e
nurhusein <nurabd@student.chalmers.se>
2019-04-03 14:46:16 +0200

    connection btw db

--- src/main/java/com/example/resumegeneratorbackend/model/Users.java
+++ src/main/java/com/example/resumegeneratorbackend/model/Users.java
@@ -3,10 +3,7 @@ package com.example.resumegeneratorbackend.model;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class Users {
@@ -24,8 +21,50 @@ public class Users {
     @Column(name = "password")
     private String password;
 
+    @Column(name = "PHONE")
+    private String phone;
+
+    @Column(name = "ADMINORUSER")
+    private boolean adminOrUser;
+
+    @Column(name = "Description")
+    private String Description;
+
+    @Column(name = "Image")
+    private int Image;
+
+
+    @JoinColumn(name = "EDUCATION_id", referencedColumnName = "id")
+    @ManyToOne
+    private Education education_id;
+
+    @JoinColumn(name = "WORKEXPERIENCE_id", referencedColumnName = "id")
+    @ManyToOne
+    private Workexperience workexperience_id;
+
+    @JoinColumn(name = "SKILLS_id", referencedColumnName = "id")
+    @ManyToOne
+    private Skills skills_id;
+
+    @JoinColumn(name = "LANGUAGES_id", referencedColumnName = "id")
+    @ManyToOne
+    private Languages languages_id;
+
+    @JoinColumn(name = "OTHERS_id", referencedColumnName = "id")
+    @ManyToOne
+    private Others others_id;
+
+    @JoinColumn(name = "COURSES_id", referencedColumnName = "id")
+    @ManyToOne
+    private  Courses courses_id;
+
+
+
+
+
 
     public String getEmail() {
+
         return email;
     }
 
@@ -56,4 +95,5 @@ public class Users {
     public void setPassword(String password) {
         this.password = password;
     }
+
 }
