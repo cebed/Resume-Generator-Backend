bdf355189d6613a4266ba80b673895b8d0a3cafb
nurhusein <nurabd@student.chalmers.se>
2019-04-03 21:32:33 +0200

    we can get proper data from front end

--- src/main/java/com/example/resumegeneratorbackend/ResumegeneratorBackendApplication.java
+++ src/main/java/com/example/resumegeneratorbackend/ResumegeneratorBackendApplication.java
@@ -2,6 +2,8 @@ package com.example.resumegeneratorbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ResumegeneratorBackendApplication {
@@ -9,5 +11,13 @@ public class ResumegeneratorBackendApplication {
     public static void main(String[] args) {
         SpringApplication.run(ResumegeneratorBackendApplication.class, args);
     }
-
+    @Bean
+    public FilterRegistrationBean corsFilterRegistration() {
+        FilterRegistrationBean registrationBean =
+                new FilterRegistrationBean(new CORSFilter());
+        registrationBean.setName("CORS Filter");
+        registrationBean.addUrlPatterns("/*");
+        registrationBean.setOrder(1);
+        return registrationBean;
+    }
 }
