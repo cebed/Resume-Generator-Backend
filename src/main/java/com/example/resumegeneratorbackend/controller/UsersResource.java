4c73b56ed43830e8e297d640c1b683d81939bc91
nurhusein <nurabd@student.chalmers.se>
2019-04-03 10:30:42 +0200

    temporaly login method

--- src/main/java/com/example/resumegeneratorbackend/controller/UsersResource.java
+++ src/main/java/com/example/resumegeneratorbackend/controller/UsersResource.java
@@ -1,6 +1,7 @@
 package com.example.resumegeneratorbackend.controller;
 
 import com.example.resumegeneratorbackend.model.Users;
+
 import com.example.resumegeneratorbackend.repository.UsersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -17,24 +18,38 @@ public class UsersResource {
 
     @GetMapping(value = "/all")
     public List<Users> getAll() {
+
         return usersRepository.findAll();
     }
 
-    @PostMapping(path = "/load", produces = MediaType.APPLICATION_XML_VALUE)
-    public void persist(@RequestBody  Users users) {
 
-        usersRepository.save(users);
-
-    }
 
     // denna metoden gör att man kan registerara sig genom enadst att skriva email.
     //anledningen är att att jag har misslyckat att skicka över ett object från front enden som motsvar
     // user
-    @PostMapping(path = "/email", produces = MediaType.APPLICATION_XML_VALUE)
-    public void Register(@RequestBody String email) {
+    @PostMapping(path = "/register", produces = MediaType.APPLICATION_XML_VALUE)
+    public String Register(@RequestBody String email) {
         Users n = new Users();
         n.setEmail(email);
+
         usersRepository.save(n);
+
+
+
+        return "hej";
+    }
+
+    @PostMapping(path = "/login", produces = MediaType.APPLICATION_XML_VALUE)
+    public String Login(@RequestBody String email) {
+        String finns = " ";
+        for(Users u : usersRepository.findAll()){
+            if(email.equals(u.getEmail())) {
+                finns =  u.getEmail();
+            }
+
+        }
+        System.out.println("???????????????????????????????????????????????????????????????" + finns);
+        return "response";
     }
 
 
