7772d504a8f54587c721e6e2ef8cc685a7b35476
nurhusein <nurabd@student.chalmers.se>
2019-04-15 09:40:15 +0200

    added new func to workexp

--- src/main/java/com/example/resumegeneratorbackend/controller/WorkExController.java
+++ src/main/java/com/example/resumegeneratorbackend/controller/WorkExController.java
@@ -1,25 +1,17 @@
 package com.example.resumegeneratorbackend.controller;
 
 import com.example.resumegeneratorbackend.model.Users;
-import com.example.resumegeneratorbackend.payloads.JWTLoginSuccessRes;
-import com.example.resumegeneratorbackend.payloads.LoginRequests;
-import com.example.resumegeneratorbackend.repository.UsersRepository;
-import com.example.resumegeneratorbackend.security.JwtTokenProvider;
-import com.example.resumegeneratorbackend.service.UserService;
+import com.example.resumegeneratorbackend.model.Workexperience;
+
+import com.example.resumegeneratorbackend.repository.WorkRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.validation.BindingResult;
+
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
 
-import static com.example.resumegeneratorbackend.security.SecurityCockpit.TOKEN_PREFIX;
+
 
 @RestController
 @CrossOrigin
@@ -27,23 +19,23 @@ import static com.example.resumegeneratorbackend.security.SecurityCockpit.TOKEN_
 public class WorkExController {
 
     @Autowired
-    private UserService userServices;
+    private WorkRepository workRepository;
 
 
 
 
     @GetMapping(value = "/all")
-    public List<Users> getAll() {
+    public List<Workexperience> getAll() {
 
-        return userServices.getAllUsers();
+        return workRepository.findAll();
     }
 
 
 
     @PostMapping("/register")
-    public Users registerUser(@Valid @RequestBody Users user){
+    public Workexperience registerUser(@Valid @RequestBody Workexperience workexperience){
 
-        return  userServices.saveUser(user);
+        return  workRepository.save(workexperience);
 
     }
 
