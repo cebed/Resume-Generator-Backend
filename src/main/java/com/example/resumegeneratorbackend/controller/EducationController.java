062aa7dce9c0edf23cb437d40d1ab7e50294b5a1
nurhusein <nurabd@student.chalmers.se>
2019-05-08 12:59:43 +0200

    refaktoring 3

--- src/main/java/com/example/resumegeneratorbackend/controller/EducationController.java
+++ src/main/java/com/example/resumegeneratorbackend/controller/EducationController.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.List;
 import java.util.Optional;
 
 
@@ -22,50 +21,29 @@ public class EducationController {
     @Autowired
     private EducationService educationService;
 
-
-
-
     @GetMapping(value = "/all")
     public Iterable<Education> getAll() {
-
         return educationService.getAll();
     }
 
-
-
     @PostMapping("/reg")
     public Education register(@Valid @RequestBody Education education){
-
-
         return  educationService.Register(education);
-
     }
-
-
     @GetMapping("/EduById/{id}")
     public Optional<Education> EducationById(@PathVariable int id){
-System.out.println("------------------------------------------------------------------");
         return  educationService.EducationById(id);
-
     }
-
-
-
 
     @PutMapping ("/update/{id}")
     public Education updateWorkEx(@RequestBody Education u, @PathVariable int id) {
-
         return educationService.updateCourse(u , id);
-
     }
 
 
     @DeleteMapping  ("/delete/{id}")
     public String delete(@PathVariable int id) {
-        System.out.println("________________---------------------");
-
         return educationService.deleteWorExpeience(id);
-
     }
 
 }
